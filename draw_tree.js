b6fa13b87b8bba97c9a7c46710c2b2972c550f4b
unknown <a3302a0229@gmail.com>
2021-01-29 00:37:18 +0800

    add html and js

---
+++ draw_tree.js
@@ -0,0 +1,27 @@
+let canvas = document.getElementById("treeCanvas");
+let cx = canvas.getContext("2d");
+cx.translate(800,600); 
+cx.rotate(180*Math.PI/180)
+cx.translate(400,0)
+//cx.translate(400,0)//變更原點位置 (0,0) -> (x,y)
+branch(22, 0.5, 0.8);
+
+function branch(length, angle, scale) {
+    if (length < 30){
+      cx.fillStyle = 'brown'
+      cx.fillRect(0, 0, 2, length);
+    }
+    else{
+      cx.fillStyle = 'brown'
+      cx.fillRect(0, 0, 2, length); //畫長方形(x,y,width,height)
+    }
+
+    if (length < 10) return;
+    cx.save();
+    cx.translate(0, length);
+    cx.rotate(-angle);
+    branch(length * scale, angle, scale);
+    cx.rotate(2 * angle);
+    branch(length * scale, angle, scale);
+    cx.restore();
+  }
